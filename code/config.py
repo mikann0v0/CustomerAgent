43a8cbc42f04588f92f6057d4bf0dd7f75027dc8
mikann0v0 <houcj1224@gmail.com>
2026-04-01 13:39:14 +0800

    delete some files

--- code/config.py
+++
@@ -1,9 +0,0 @@
-CHAT_MODEL = "qwen/qwen3.6-plus-preview:free"
-EMBEDDING_MODEL = "Qwen/Qwen3-Embedding-8B"
-VISION_MODEL = "Qwen/Qwen3-VL-8B-Instruct"
-
-OPENROUTER_API_KEY = "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"
-OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
-
-SILICON_API_KEY = "sk-cekenajymugklelyrbxjueiiklhjxprjxihaicvqnjpodflg"
-SILICON_BASE_URL = "https://api.siliconflow.com/v1"
